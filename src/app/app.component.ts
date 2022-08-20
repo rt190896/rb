4f729fa510989889d489abdc52bf03a3950593dc
Ronak Trivedi <ronak.trivedi@thesunflowerlab.com>
2022-08-20 14:39:14 +0530

    changes_for_data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Formula-changes';
+  width: number = 0;
+  length: number = 0;
+  Profile: number = 0;
+  Qty: number = 0;
+
+  calculate() {
+    debugger;
+    return (this.width * this.Profile / 2) * this.Qty
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductionSlipComponent } from './production-slip/production-slip.component';
+import { StockSlipComponent } from './stock-slip/stock-slip.component';
+import { ProfileTypeComponent } from './profile-type/profile-type.component';
+import { PlanSheetComponent } from './plan-sheet/plan-sheet.component';
+import { ProductionDetailComponent } from './production-detail/production-detail.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductionSlipComponent,
+    StockSlipComponent,
+    ProfileTypeComponent,
+    PlanSheetComponent,
+    ProductionDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
